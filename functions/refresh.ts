fcb3e33c5ca8c22e9ef47c1007ef9706c1e66d97
Douglas <8209+douglasjarquin@users.noreply.github.com>
2024-12-08 13:46:28 -0500

    Delete the unused update function

--- functions/refresh.ts
+++ functions/refresh.ts
@@ -2,26 +2,32 @@ interface Context {
   env: {
     AIRTABLE_ACCESS_TOKEN: string
     AIRTABLE_BASE_ID: string
+    API_TOKEN: string
   }
+  request: Request
 }
 
 async function getWebhooks(baseId: string, accessToken: string) {
   let url = `https://api.airtable.com/v0/bases/${baseId}/webhooks`
   let payload = {
     headers: {
-      "Authorization": `Bearer ${accessToken}`
+      Authorization: `Bearer ${accessToken}`,
     },
   }
   let response = await fetch(url, payload)
   return response
 }
 
-async function refreshWebhook(webhookId: string, baseId: string, accessToken: string) {
+async function refreshWebhook(
+  webhookId: string,
+  baseId: string,
+  accessToken: string
+) {
   let url = `https://api.airtable.com/v0/bases/${baseId}/webhooks/${webhookId}/refresh`
   let payload = {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Authorization": `Bearer ${accessToken}`,
+      Authorization: `Bearer ${accessToken}`,
     },
   }
   let response = await fetch(url, payload)
@@ -29,13 +35,27 @@ async function refreshWebhook(webhookId: string, baseId: string, accessToken: st
 }
 
 export async function onRequestGet(context: Context) {
-  let webhooks = await getWebhooks(context.env.AIRTABLE_BASE_ID, context.env.AIRTABLE_ACCESS_TOKEN)
+  const url = new URL(context.request.url)
+  const token = url.searchParams.get('token')
+
+  if (token !== context.env.API_TOKEN) {
+    return new Response('Unauthorized', { status: 401 })
+  }
+
+  let webhooks = await getWebhooks(
+    context.env.AIRTABLE_BASE_ID,
+    context.env.AIRTABLE_ACCESS_TOKEN
+  )
   let json = await webhooks.json()
   let ids = json.webhooks.map((webhook: any) => webhook.id)
 
   for (let id of ids) {
     // console.log(`Refreshing webhook ${id}`)
-    await refreshWebhook(id, context.env.AIRTABLE_BASE_ID, context.env.AIRTABLE_ACCESS_TOKEN)
+    await refreshWebhook(
+      id,
+      context.env.AIRTABLE_BASE_ID,
+      context.env.AIRTABLE_ACCESS_TOKEN
+    )
   }
-  return new Response("Success!", { status: 200 });
+  return new Response('Success!', { status: 200 })
 }
